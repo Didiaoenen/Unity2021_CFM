c5485d7a1bceadfeeaa3453a6ff037cd89752300
Didiaoenen <1191430879@qq.com>
2024-03-01 23:31:24 +0800

    commit add

--- Assets/Script/Simple/Interactivity/IInteractionAction.cs
+++
@@ -1,8 +0,0 @@
-namespace Assembly_CSharp.Assets.Script.Simple.Interactivity
-{
-    public interface IInteractionAction
-    {
-        void OnRequest(object sender, InteractionEventArgs args);
-    }
-}
-
